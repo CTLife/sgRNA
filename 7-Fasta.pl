0a2b13da922218cd8325260440c812ffd424c8c6
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2016-05-09 17:20:57 +0800

    Create 7-Fasta.pl

---
+++ 7-Fasta.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/env perl5
+use  strict;
+use  warnings;
+use  v5.18;
+use  List::UtilsBy qw( nsort_by );
+
+
+my $inputDir = "6-Top10";
+my $outDir   = "7-Fasta";
+opendir(my $DH_input, $inputDir)  ||  die;     
+my @inputFiles_g = readdir($DH_input);
+sub myMakeDir  {
+    my $path = $_[0];
+    if ( !( -e $path) )  { system("mkdir  -p  $path"); }
+    if ( !( -e $path) )  { mkdir $path  ||  die; }
+}
+&myMakeDir($outDir);
+
+
+for ( my $i=0; $i<=$#inputFiles_g; $i++ ) {   
+        next unless $inputFiles_g[$i] =~ m/\.fa$/;  
+        next unless $inputFiles_g[$i] !~ m/^[.]/;
+        next unless $inputFiles_g[$i] !~ m/[~]$/;
+        #say   "\t$inputFiles_g[$i] ... ..." ; 
+        my $temp1 = "$outDir/$inputFiles_g[$i]";
+        open(inputFILE1, "<", "$inputDir/$inputFiles_g[$i]") or die "$!"; 
+        open(outFILE1,   ">", $temp1)   or die "$!"; 
+        my @lines1 = <inputFILE1>;
+        if($#lines1<8) {say   $inputFiles_g[$i];  }      
+
+        for (my $i1=1; $i1<=$#lines1; $i1++) { 
+                  $lines1[$i1] =~ m/^(\S+)\s+(\S+)\s+/ or die;         
+                  my $sgRNA = $1;
+                  my $name  = $2;
+                  $lines1[$i1] =~ s/\s/_/g or die; 
+                  print  outFILE1  ">$name.$lines1[$i1]\n";
+                  print  outFILE1  "$sgRNA\n";
+        }
+}
+
+
+
+
+
+
+
+
+

d1a39778c764d9c9ec67886ee9c8093f7c6e0954
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2016-05-09 17:23:04 +0800

    Create 10-Format.pl

---
+++ 10-Format.pl
@@ -0,0 +1,139 @@
+#!/usr/bin/env perl5
+use  strict;
+use  warnings;
+use  v5.18;
+use  List::UtilsBy qw( nsort_by );
+
+
+my $inputDir = "9-offTarget";
+opendir(my $DH_input, $inputDir)  ||  die;     
+my @inputFiles_g = readdir($DH_input);
+
+sub myMakeDir  {
+    my $path = $_[0];
+    if ( !( -e $path) )  { system("mkdir  -p  $path"); }
+    if ( !( -e $path) )  { mkdir $path  ||  die; }
+}
+my $outDir   = "10-Format";
+my $tempDir  = "yp-temp-for-10-Format";
+&myMakeDir($outDir);
+&myMakeDir($tempDir);
+
+
+
+
+
+if (0==0) {
+for ( my $i=0; $i<=$#inputFiles_g; $i++ ) {   
+        next unless $inputFiles_g[$i] =~ m/\.fa$/;  
+        next unless $inputFiles_g[$i] !~ m/^[.]/;
+        next unless $inputFiles_g[$i] !~ m/[~]$/;
+        open(inputFILE1, "<", "$inputDir/$inputFiles_g[$i]/OfftargetAnalysis.xls") or die "##\n\n$inputFiles_g[$i]\n\n##"; 
+        my @lines1 = <inputFILE1>;
+        &myMakeDir("$tempDir/$inputFiles_g[$i]");
+
+        for (my $j=1;  $j<=$#lines1;  $j++) { 
+            #$lines1[$j] =~ s/\"//g  or  die;
+            #$lines1[$j] !~ m/\"/    or  die;
+            $lines1[$j] =~ m/^(\S+)\s+/ or die;  
+            my $name = $1;
+            $name =~ s/\"//g  or  die;
+            $name !~ m/\"/    or  die;
+            open(outFILE1,   ">>", "$tempDir/$inputFiles_g[$i]/$name")   or die "$!";    
+            print   outFILE1   $lines1[$j];
+        }
+
+}
+}
+
+
+
+
+opendir(my $DH_temp, $tempDir)  ||  die;     
+my @tempFiles_g = readdir($DH_temp);
+open(FILEremove,   ">",  "$outDir/removed_sgRNAs.txt")   or die "$!";  
+
+
+for ( my $i=0; $i<=$#tempFiles_g; $i++ ) {   
+        next unless $tempFiles_g[$i] =~ m/^chr/;  
+        next unless $tempFiles_g[$i] =~ m/fa$/;  
+        next unless $tempFiles_g[$i] !~ m/^[.]/;
+        next unless $tempFiles_g[$i] !~ m/[~]$/;
+        opendir(my $DH_temp1, "$tempDir/$tempFiles_g[$i]")  ||  die;     
+        my @tempFiles1 = readdir($DH_temp1);
+        open(inputFILE1, "<", "$inputDir/$tempFiles_g[$i]/OfftargetAnalysis.xls") or die "$!"; 
+        my @lines1 = <inputFILE1>;
+        open(FILE2,   ">",  "$outDir/$tempFiles_g[$i]")   or die "$!";  
+        print     FILE2      "sgRNA\tPAM\tOfftargetScore\t$lines1[0]";
+
+        for (my $j=0;  $j<=$#tempFiles1;  $j++) {   ## one cycle is one folder (one enhancer).
+            next unless $tempFiles1[$j] =~ m/^chr/;  
+            #next unless $tempFiles1[$j] =~ m/__$/;  
+            next unless $tempFiles1[$j] !~ m/^[.]/;
+            next unless $tempFiles1[$j] !~ m/[~]$/;
+            open(FILE1,   "<",  "$tempDir/$tempFiles_g[$i]/$tempFiles1[$j]")   or   die "$!";    
+            my @tempLines1 = <FILE1>;  
+            my $score = 100; 
+            my $top10score = 0; 
+            my $bool  = 0;
+            my $index = "";
+            for (my $k=0;  $k<=$#tempLines1;  $k++) {  ## one cycle is one sgRNA.
+                 say   $tempLines1[$k];
+                 $tempLines1[$k] =~ m/^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+/  or  die; 
+                 my $name1    = $1;
+                 my $gRNAPAM1 = $2;
+                 my $score1   = $4;       
+                 my $chrom1   = $11;       
+                 my $start1   = $12;       
+                 my $end1     = $13; 
+                 ($score >= $score1)  or  die; 
+                 $score = $score1;      
+                 if($k==0) {($score1 == 100)  or die; }     
+                 if($k<=10) {$top10score = $top10score+$score1; }
+                 $name1 =~ m/^\"(chr[\dXY]+):(\d+)\-(\d+)_/  or  die; 
+                 my $chrom2 = $1;    
+                 my $start2 = $2;    
+                 my $end2   = $3; 
+                 $gRNAPAM1 !~ m/TTTTT/  or  die;    
+                 $gRNAPAM1 =~ m/^\"(G[AGCTN]{19})([AGCTN]GG)\"$/  or  die;    
+                 my $sgRNA = $1;
+                 my $PAM = $2;
+                 $chrom1 =~ s/\"//g    or  die;
+                 $chrom1 =~ m/^chr/    or  die;
+                 if( ($bool == 0) and ($chrom1 eq $chrom2) and ($start2 <= $start1) and ($end2 >= $end1) ) {
+                       ($score1 == 100)   or  die "##\n\n$tempLines1[$k]\n\n##";
+                       $top10score = $top10score - $score1;
+                       ##print  FILE2  "$sgRNA\t$PAM\t$top10score\t$tempLines1[$k]";   
+                       $bool++;
+                       $index = $k;
+                 }  
+                 if($k==$#tempLines1) { 
+                     if($bool == 1) {  
+                         print  FILE2  "$sgRNA\t$PAM\t$top10score\t$tempLines1[$index]";  
+                     }else{
+                         ($bool == 0)  or  die;
+                         print  FILEremove  "$sgRNA\t$PAM\t$top10score\t$tempLines1[0]";  
+                     }                          
+                 }
+            }                      
+        }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
